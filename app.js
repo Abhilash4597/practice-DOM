c062f0fbbbcd582ba608d15c27f0c17fd02f7866
Abhilash4597 <abhilash4597@gmail.com>
2023-02-12 15:19:34 +0530

    counter using one button

--- app.js
+++ app.js
@@ -3,21 +3,39 @@ const minus = document.querySelector('.minus');
 const count = document.querySelector('.count');
 let counting = 0;
 
-function increase() {
-  counting++;
-  count.textContent = counting;
-  minus.disabled = false;
-}
+// function increase() {
+//   counting++;
+//   count.textContent = counting;
+//   minus.disabled = false;
+// }
 
-function decrease() {
-  if (counting > 0) {
-    counting--;
+// function decrease() {
+//   if (counting > 0) {
+//     counting--;
+//     count.textContent = counting;
+//   } else {
+//     minus.disabled = true;
+//   }
+// }
+
+// plus.addEventListener('click', increase);
+
+// minus.addEventListener('click', decrease);
+
+function countingAndDisplaying() {
+
+  if (minus.textContent == 'increase') {
+    counting++;
     count.textContent = counting;
   } else {
-    minus.disabled = true;
+    counting--;
+    count.textContent = counting;
+  }
+
+  if (count.textContent == 10) {
+    minus.textContent = 'decrease'
+  } else if (count.textContent == 0) {
+    minus.textContent = 'increase';
   }
 }
-
-plus.addEventListener('click', increase);
-
-minus.addEventListener('click', decrease);
+minus.addEventListener('click', countingAndDisplaying);
