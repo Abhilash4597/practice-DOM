3e51444cf523c24269e52a72ab46669bc674358c
Abhilash4597 <abhilash4597@gmail.com>
2023-02-12 14:47:07 +0530

    counter using dom

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-console.log(document);
-const ele = document.getElementById("heading1");
-ele.style.backgroundColor = "red";
+const plus = document.querySelector('.plus');
+const minus = document.querySelector('.minus');
+const count = document.querySelector('.count');
+let counting = 0;
+
+function increase() {
+  counting++;
+  count.textContent = counting;
+  minus.disabled = false;
+}
+
+function decrease() {
+  if (counting > 0) {
+    counting--;
+    count.textContent = counting;
+  } else {
+    minus.disabled = true;
+  }
+}
+
+plus.addEventListener('click', increase);
+
+minus.addEventListener('click', decrease);
